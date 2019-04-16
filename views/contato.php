967bb797186e17497d143a34331e0492f2066b93
Hugo Leone de Araújo Souza <hugoleone.dev@gmail.com>
2019-04-16 12:34:47 -0400

    Novas alterações no menu "contato"

--- views/contato.php
+++ views/contato.php
@@ -9,22 +9,22 @@
 			<div class="col-md-6 col-md-offset-3"	>
 				<img src="../img/logo.png" class="img-responsive center">	
 					<table class="table" style="width:100%">
-					    <tr>
+					    <tr style="border: 1px solid #ccc">
 					     	<th>Nome</th>
 					     	<th><i class="fa fa-mobile" aria-hidden="true" style="font-size: 25px; color: #708090; margin-left: 10px;"> Ligue!</i></th>
 					    	<th><i class="fa fa-thumbs-o-up" aria-hidden="true" style="font-size: 25px; color: #4267b2;"> Siga!</i></th>
 					  	</tr>
-					  	<tr>
+					  	<tr style="border: 1px solid #ccc">
 					    	<td>Hugo Leone de Araújo Souza</td>
 					    	<td><a href="tel:92 993644241">(92) 99364-4241</a></td>
 					    	<td><a href="#">Hugo Leone</a></td>
 					  	</tr>
-					  	<tr>
+					  	<tr style="border: 1px solid #ccc">
 					    	<td>Flávia Fernandes Souza</td>
 					    	<td><a href="tel:92 992038432">(92) 99203-8432</a></td>
 					    	<td><a href="https://www.facebook.com/flavinha.nandes">Flávia Fernandes</a></td>
 					  	</tr>
-					  	<tr>
+					  	<tr style="border: 1px solid #ccc">
 					    	<td>Paulo Sérgio Lira de Souza</td>
 					    	<td><a href="tel:92 993731477">(92) 99373-1477</a></td>
 					    	<td><a href="https://www.facebook.com/paulosergio.pool">Paulo Sérgio</a></td>
