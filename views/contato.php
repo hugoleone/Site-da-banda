4c83ad8e9ecaee651db98d7b4bff4880b899dda5
hugoleone <hugoleone.dev@gmail.com>
2019-04-15 20:41:16 -0400

    Adicionando tag para ligações via smartphone

---
+++ views/contato.php
@@ -0,0 +1,37 @@
+<div id="main" class="center">
+	<!-- <span style="color: #9bbd46">--Sobre--</span> -->
+	<h2>Contatos</h2>
+</div>
+
+<div id="sobre">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6 col-md-offset-3"	>
+				<img src="../img/j4.jpg" class="img-thumbnail img-responsive center">	
+					<table style="width:100%">
+					    <tr>
+					     	<th>Nome</th>
+					     	<th>Telefone</th> 
+					    	<th>E-mail</th>
+					  	</tr>
+					  	<tr>
+					    	<td>Hugo Leone de Araújo Souza</td>
+					    	<td><a href="tel:92 99364-4241">(92) 99364-4241</a></td>
+					    	<td>50</td>
+					  	</tr>
+					  	<tr>
+					    	<td>Eve</td>
+					    	<td>Jackson</td>
+					    	<td>94</td>
+					  	</tr>
+					  	<tr>
+					    	<td>John</td>
+					    	<td>Doe</td>
+					    	<td>80</td>
+					  	</tr>
+					</table>
+				<p style="text-align: center"><a href="?i=navegar" class="btn btn-primary btn-lg">Navegar</a></p>
+			</div>
+		</div>	
+	</div>	
+</div>
